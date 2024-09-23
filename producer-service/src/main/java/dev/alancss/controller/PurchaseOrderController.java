5554c88231095ecf753a4f51698a6085b79a4842
Alan Silva <alanclash2541@gmail.com>
2024-09-23 01:41:17 -0300

    feat: add batch insertion endpoint to simulate Kafka events in bulk

--- producer-service/src/main/java/dev/alancss/controller/PurchaseOrderController.java
+++ producer-service/src/main/java/dev/alancss/controller/PurchaseOrderController.java
@@ -4,10 +4,7 @@ import dev.alancss.event.Order;
 import dev.alancss.producer.PurchaseOrderProducer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/purchase-orders")
@@ -22,6 +19,12 @@ public class PurchaseOrderController {
     @PostMapping
     public ResponseEntity<?> sendEvent(@RequestBody Order order) {
         purchaseOrderProducer.sendPurchaseOrder(order);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/batch")
+    public ResponseEntity<?> sendEventInBatch(@RequestParam int batchSize) {
+        purchaseOrderProducer.sendPurchaseOrderInBatch(batchSize);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- producer-service/src/main/java/dev/alancss/producer/PurchaseOrderProducer.java
+++ producer-service/src/main/java/dev/alancss/producer/PurchaseOrderProducer.java
@@ -7,14 +7,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.UUID;
 
 @Slf4j
 @Service
 public class PurchaseOrderProducer {
 
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
     private final KafkaTemplate<String, PurchaseOrderEvent> kafkaTemplate;
+    private final Random random = new Random();
 
     public PurchaseOrderProducer(KafkaTemplate<String, PurchaseOrderEvent> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -29,4 +34,22 @@ public class PurchaseOrderProducer {
         kafkaTemplate.send("purchase-orders", purchaseOrderEvent);
     }
 
+    public void sendPurchaseOrderInBatch(int batchSize) {
+        for (int i = 0; i < batchSize; i++) {
+            String orderId = UUID.randomUUID().toString();
+            String customerId = UUID.randomUUID().toString();
+            double amount = random.nextDouble() * 200 - 100;
+            var randomDate = LocalDateTime.now().minusDays(random.nextInt(30));
+            String date = randomDate.format(formatter);
+
+            var order = new Order(orderId, customerId, amount, getRandomStatus(), date);
+
+            sendPurchaseOrder(order);
+        }
+    }
+
+    private String getRandomStatus() {
+        String[] statuses = {"PAID", "PENDING", "FAILED", "ERROR"};
+        return statuses[random.nextInt(statuses.length)];
+    }
 }
